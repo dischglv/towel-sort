aa58d65793f94f11988a45babc2b7aa2b2d293ef
Diana Schegoleva <dschegoleva@gmail.com>
2021-03-05 19:18:30 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!matrix || (matrix.length == 0)) return [];
+
+  return matrix.reduce((pM, cM, i) => (i%2 == 0) ? pM.concat(cM) : pM.concat(cM.reverse()));
 }
+
+
